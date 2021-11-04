e3e3bc40e1288722920b240836af02014f463c68
Emir <emirayhanboii@icloud.com>
2021-11-04 12:42:09 +0600

    я сделал два слайда :)

---
+++ app/src/main/java/com/example/onlineteachingplatform/emir_2.kt
@@ -0,0 +1,11 @@
+package com.example.onlineteachingplatform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class emir_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emir2)
+    }
+}
