fe33b1d0402d1f82dcde888f9302c3f3f90d7c65
umarabv <umar.abv0311@gmail.com>
2021-12-19 00:19:06 +0600

    kds

--- app/src/main/java/com/example/onlineteachingplatform/emir_2.kt
+++ app/src/main/java/com/example/onlineteachingplatform/emir_2.kt
@@ -1,11 +1,19 @@
 package com.example.onlineteachingplatform
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class emir_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emir2)
+        val btn = findViewById<TextView>(R.id.Sen1)
+
+        btn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
